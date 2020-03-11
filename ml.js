b8fb1831ed9dcdc13af074b6252146d88fba7bd1
HarjotSingh8 <harjotsinghsandhu800@gmail.com>
2020-03-12 00:18:55 +0530

    implementing add conncetion

--- ml.js
+++ ml.js
@@ -24,6 +24,20 @@ class Connection {
   }
 }
 
+let v;
+function testSpecies() {
+  v = new Species(5, 3);
+  v.mutateAddConnection();
+  v.mutateAddConnection();
+  v.mutateAddConnection();
+  v.mutateAddConnection();
+  v.mutateAddConnection();
+  v.mutateAddNode();
+  v.mutateAddNode();
+  v.mutateAddNode();
+  v.mutateAddNode();
+  return v;
+}
 class Species {
   constructor(numInputs, numOutputs) {
     this.numInputs = numInputs;
@@ -44,42 +58,72 @@ class Species {
       this.genes.push(this.outputs[i]);
     }
     this.numGenes = this.numInputs + this.numOutputs;
-    this.numConnections = 0;
+    this.numConnections = 0; //aka innovation number
     this.connections = [];
+    this.maxDepth = 1;
   }
   mutate() {
     //mutation here
   }
   adjustDepth(depthLevel) {
     //add a new level at depthlevel
+    //Complete
+    //Not Tested
     for (let i = 0; i < this.numGenes; i++) {
       if (this.genes[i].depth >= depthLevel) {
         this.genes[i].depth++;
       }
     }
+    this.maxDepth = this.genes[this.numInputs].depth;
   }
   mutateAddConnection() {
     //add a connection between unconnected nodes
-
-    let randomSourceNodeIndex = random(0, this.numGenes);
-    while (this.genes[randomSourceNodeIndex].depth < 1) {
-      randomSourceNodeIndex = random(0, this.numGenes);
-    }
-    let randomNodeIndex = random(0, this.numGenes);
-
-    while (
-      this.genes[randomSourceNodeIndex].depth <=
-        this.genes[randomNodeIndex].depth ||
-      !this.checkConnection(
-        this.genes[randomSourceNodeIndex],
-        this.genes[randomNodeIndex]
-      )
-    ) {
-      randomSourceNodeIndex = random(0, this.numGenes);
-      randomNodeIndex = random(0, this.numGenes);
+    //Complete
+    //Not Tested
+    let tries = 20;
+    while (tries > 1) {
+      let randomSourceNodeIndex = parseInt(random(0, this.numGenes - 1));
+      while (this.genes[randomSourceNodeIndex].depth < 1) {
+        randomSourceNodeIndex = parseInt(random(0, this.numGenes));
+      }
+      let randomNodeIndex = parseInt(random(0, this.numGenes - 1));
+      let tryCount = 20;
+      while (
+        tryCount > 0 &&
+        (this.genes[randomSourceNodeIndex].depth >=
+          this.genes[randomNodeIndex].depth ||
+          this.checkConnection(
+            this.genes[randomSourceNodeIndex],
+            this.genes[randomNodeIndex]
+          ))
+      ) {
+        randomSourceNodeIndex = parseInt(random(0, this.numGenes));
+        tryCount--;
+      }
+      if (
+        this.genes[randomSourceNodeIndex].depth <
+          this.genes[randomNodeIndex].depth &&
+        !this.checkConnection(
+          this.genes[randomSourceNodeIndex],
+          this.genes[randomNodeIndex]
+        )
+      ) {
+        //add new connection here
+        let newConnection = new Connection(
+          this.genes[randomSourceNodeIndex],
+          this.genes[randomNodeIndex],
+          this.numConnections
+        );
+        this.connections.push(newConnection);
+        this.numConnections++;
+        break;
+      }
+      tries--;
     }
   }
   checkConnection(source, node) {
+    //Complete
+    //Not Tested
     for (let i = 0; i < this.numConnections; i++) {
       if (
         this.connections[i].source.id == source.id &&
@@ -91,12 +135,27 @@ class Species {
   }
   mutateAddNode() {
     //new connection here
-    let randomConnectionIndex = random(0, this.numInnovations - 1);
+    //Complete
+    //Not Tested
+    let randomConnectionIndex = parseInt(random(0, this.numConnections - 1));
     while (!this.connections[randomConnectionIndex].enabled) {
-      randomConnectionIndex = random(0, this.numInnovations - 1);
+      randomConnectionIndex = parseInt(random(0, this.numConnections - 1));
     }
+    if (
+      this.connections[randomConnectionIndex].node.depth -
+        this.connections[randomConnectionIndex].source.depth ==
+      1
+    ) {
+      this.adjustDepth(this.connections[randomConnectionIndex].node.depth);
+      //adjust depth
+    }
+    let depth = parseInt(
+      (this.connections[randomConnectionIndex].node.depth +
+        this.connections[randomConnectionIndex].source.depth) /
+        2
+    );
     this.connections[randomConnectionIndex].enabled = false;
-    let newGene = new Node();
+    let newGene = new Node(this.numGenes++, depth, "hidden");
     this.genes.push(newGene);
     this.connections.push(
       new Connection(
@@ -112,14 +171,6 @@ class Species {
         this.numConnections++
       )
     );
-    if (
-      this.connections[randomConnectionIndex].node.depth -
-        this.connections[randomConnectionIndex].source.depth ==
-      1
-    ) {
-      this.adjustDepth(this.connections[randomConnectionIndex].node.depth);
-    }
-    //adjust
   }
 }
 
